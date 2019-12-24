b047b5f2ffdb83aa6c8a06c3485c2ebe251de6cf
igor <igorbasket93@gmail.com>
2019-12-24 18:43:54 +0200

    class work

---
+++ src/main/groovy/com/trainingapp/trainingapp/model/NewTrainingDTO.groovy
@@ -0,0 +1,12 @@
+package com.trainingapp.trainingapp.model
+
+import java.time.LocalDateTime
+
+class NewTrainingDTO {
+
+    String name
+
+
+
+
+}
--- trainingvue/src/App.vue
+++ trainingvue/src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <UsersList msg="My Training APP"/>
+    <img alt="Vue logo" src="./assets/man.png" width="200px" height="200px">
+    <!--    <img alt="Vue logo" src="./assets/logo.png">-->
+    <!--    <UsersList msg="My Training APP"/>-->
+    <h1>My-TrainingAPP</h1>
+    <hr>
+<!--    <router-link to="/">Home</router-link> <router-link to="/users">UsersList</router-link> -->
+    <h1></h1>
+    <router-view></router-view>
   </div>
 </template>
 
-<script>
-import UsersList from './components/UsersList.vue'
+<!--<script>-->
+<!--  import UsersList from './components/UsersList.vue'-->
+<!--  export default {-->
+<!--    name: 'app',-->
+<!--    components: {-->
+<!--      UsersList-->
+<!--    }-->
+<!--  }-->
+<!--</script>-->
 
-export default {
-  name: 'app',
-  components: {
-    UsersList
+<script>
+  export default {
+    data() {
+      return {}
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  html,
+  body{
+    background: lightcyan;
+  }
 </style>
